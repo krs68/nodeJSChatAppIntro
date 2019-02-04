2b75e4a6f2f1d54d33cd439ee44a6f00129a7cf6
rakasarla <rakasarla@yahoo.com>
2019-02-04 14:33:03 -0500

    Format timestams using moment js is done

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -9,10 +9,6 @@ describe('generateMessage', () => {
         var messageResp = generateMessage(from, text);
         expect(messageResp.from).toBe(from);
         expect(messageResp.text).toBe(text);
-        // or you can write above two lines as:
-        // --> not working --> expect(messageResp).toInclude({from, text});
-        // --> not working --> expect(messageResp.createdAt).toBeA("number");
-        // newer versions have different commands, need to refer to last lecture
     });
 });
 
@@ -22,11 +18,7 @@ describe('generateLocationMessage', () => {
         var coords = {latitude: '111111111111', longitude: '2222222222222222' };
         var url = `https://www.google.com/maps/search/?api=1&query=${coords.latitude},${coords.longitude}`;
         var messageResp = generateLocationMessage('Admin', coords.latitude, coords.longitude);
-        console.log('--------->', messageResp);
         expect(messageResp.url).toBe(url);
-        // or you can write above two lines as:
-        // --> not working --> expect(messageResp).toInclude({from, text});
-        // --> not working --> expect(messageResp.createdAt).toBeA("number");
-        // newer versions have different commands, need to refer to last lecture
+        console.log('type of testing:', messageResp.createdAt);
     });
 });
