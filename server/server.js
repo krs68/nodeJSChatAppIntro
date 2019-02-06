7e5603bbd076f651e660b22a65f9297f71d56f6e
rakasarla <rakasarla@yahoo.com>
2019-02-06 13:09:41 -0500

    Completed sending messages to paticular room number

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const {isRealString} = require('./utils/validation');
-const {Users} = require('./utils/users');
+const { isRealString } = require('./utils/validation');
+const { Users } = require('./utils/users');
 const port = process.env.PORT || 3000;
 const { generateMessage, generateLocationMessage } = require('./utils/message');
 
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.on('join', (params, callback) => {
-    if(!isRealString(params.name) || !isRealString(params.room)) {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
       return callback('Name and Room are required.');
     }
     socket.join(params.room);
@@ -36,18 +36,23 @@ io.on('connection', (socket) => {
     // socket.emit --> N/A (to specific user only)
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
     socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
-  
+
     callback();
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    var user = users.getUser(socket.id);
+    if(user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
     callback();
   });
 
   socket.on('createLocationMessage', function (coords) {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', () => {
